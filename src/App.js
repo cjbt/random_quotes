ca5206cdf54301a9750b5970ee1ad4741ad343aa
cecil john tantay <cjbtantay@gmail.com>
2018-12-15 18:55:51 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    data: {}
+  };
+
+  componentDidMount = () => {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+
+    const url =
+      "https://talaikis.com/api/quotes/random/";
+
+    fetch(proxy + url)
+      .then(res => res.json())
+      .then(data => this.setState({ data }));
+  };
+
+  
+  getQuote = () => {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+
+    const url =
+      "https://talaikis.com/api/quotes/";
+
+    fetch(proxy + url)
+      .then(res => res.json())
+      .then(data => console.log([...new Set([...data].map(index=> index.cat))]));
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h3>{data.author}</h3>
+        <p>{data.quote}</p>
+        <small>{data.cat}</small>
+
+        <button onClick={this.getQuote}>GET THIS QUOTE</button>
       </div>
     );
   }
